a67606e0000055f6e11098bb25fb27dcc97ffa2b
neeko <girl2learn2code@gmail.com>
2021-06-12 05:25:49 -0700

    lol this is actually fun.

--- play3.py
+++ play3.py
@@ -1,45 +1,40 @@
-end = 'years old'
 
+end = 'years old! Check out their popular albums: '
 
 Goat = {
-    
     'name': 'kanye',
     'age': 44,
     'albums': ['College Drop', 'Life of Pablo']
-
- 
 }
 
+
+
+
+
+
+# made user inut translate to lowercase for case sens purposes
 user = str(input("Enter a rapper ").lower())
 
 
 
 
-# to check if a value is in a dictionary if x in d.values()
+# .values is what I was looking for
 def ageCheck():
-    # what can i put in the values parameter?
-
+    # what can i put in the values parameter
+    # is there a way to only have print
     if user in Goat.values():
-        print(' is about ')
+        print('is about ')
         print(Goat['age'])
         print(end)
+        print(Goat['albums'])
+
     else:
         print('sorry ' + "'" + user + "'" + ' is not in our database! ')
 
+
+#what can i add into these parameters?
 ageCheck()
 
 
 
 
-# print (rappers['Kanye'])
-
-# def oldhead():
-
-#      if user in rappers:
-
-
-    # return rappers['Kanye']
-    # if user in rappers:
-    #     print('gj')
-    # else:
-    #     print('sorry hun')
