f386367673595ac4b323925fb1b5bb00d0059c9c
DeMoriarty <sahbanjan@gmail.com>
2021-06-13 00:41:56 +0300

    Update play3.py

--- play3.py
+++ play3.py
@@ -1,10 +1,3 @@
-
-# end = 'years old! Check out their popular albums: '
-
-#how to merge two dictionaries? but is that ideal?
-
-
-
 Rapper = {
     'name': ['kanye', 'lil uzi vert']
 }
@@ -15,38 +8,52 @@ Age = {
 }
 
 Album = {
-    'lil uzi vert': ['luv is Rapper 2', 'eternal atake'], 
-    'kanye': ['College Drop', 'Life of Pablo', 'Yeezus', 'Late Registration'],
+    'lil uzi vert': [
+      ('luv is Rapper 2', 2000),
+      ('eternal atake', 2001)
+    ], 
+    'kanye': [
+      ('College Drop', 2007),
+      ('Life of Pablo', 2005),
+      ('Yeezus', 2001),
+      ('Late Registration', 1000)
+    ],
 }
 
 
 # dirct.values 
 def RapperCheck():
-    
     # while true means loop forever!
          # made user inut translate to lowercase for case sens purposes
-        user = str(input("Enter a Rapper ").lower())
-    
-        if user in Rapper.get('name'):
-               if user in Rapper.get('name'):
+    user = str(input("Enter a Rapper ").lower())
 
-            
-                print(user)
-                
-
-
-            # i want it to only return the matching user input
-       
-
-                # print (Rapper['name'])
-
-        else:
-            print('sorry ' + "'" + user + "'" + ' is not in our Rapperbase! ')
+    # if user input is valid, then return False
+    # otherwise, return name, age and albums
+    if user in Age.keys():  
+      return (user, Age[user], Album[user])
+    else:
+      print('sorry ' + "'" + user + "'" + ' is not in our Rapperbase! ')
+      return False
 
 
 #what can i add into these parameters?
-RapperCheck()
 
+# While True loops infinitely untill break
+while True:
+  returned = RapperCheck()
 
+  # if the user input is valid, then break the loop
+  if not returned is False:
+    break
+name, age, album = returned
 
+def func(x):
+  return x[1]
 
+func = lambda x : x[1]
+
+print("before", album)
+# sorting album by years
+# lambda x : x[1] means sort album by the second item of each item of album
+album = sorted(album, key=lambda x: x[0])
+print("after", album)
